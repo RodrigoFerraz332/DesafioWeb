8ff8d546f04fb23a809ed49050527b037aba0d8b
GabrielS-newObj <gabrielsgf12@gmail.com>
2025-06-20 20:18:19 -0300

    Update script.js

--- javaScript/script.js
+++ javaScript/script.js
@@ -2,39 +2,39 @@ function verifyMsg(){
     alert("Dados enviados com sucesso 😊");
 }
 
-function verifyName(){
-    const nomeCompleto = document.getElementById("iNomeCompleto").value;
+function verifyName(id){
+    const nomeCompleto = document.getElementById(id).value;
 
-    const regex = /0-9|[^0-9]|\W/;
+    const regex = /0-9|\W/;
     const result = regex.test(nomeCompleto);
 
     if (result){
         alert("ERRO: O NOME não pode conter números ou caracteres especiais")
-        document.getElementById("iNomeCompleto").value = "";
+        document.getElementById(id).value = "";
     }
 }
 
-function verifyTopico(){
-    const topico = document.getElementById("iTopico").value;
+function verifyTopico(id){
+    const topico = document.getElementById(id).value;
 
     const regex = /\W/;
     const result = regex.test(topico);
 
     if (result){
         alert("ERRO: O TÓPICO não pode conter caracteres especiais!")
-        document.getElementById("iTopico").value = "";
+        document.getElementById(id).value = "";
     } 
 }
 
-function verifyText(){
-    const msg = document.getElementById("iMensagem").value;
+function verifyText(id){
+    const msg = document.getElementById(id).value;
 
     const regex = /\W/;
     const result = regex.test(msg);
 
     if (result){
         alert("ERRO: A MENSAGEM possui caracteres especiais")
-        document.getElementById("iMensagem").value = "";
+        document.getElementById(id).value = "";
     } 
 }
 
